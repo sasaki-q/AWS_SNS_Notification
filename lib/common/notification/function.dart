dd4268a504065d5242ddc889aea27e06cbd828e9
sasaki <teppei.sasaki@relic.co.jp>
2022-03-22 16:48:26 +0900

    Add foreground specifics permission

--- lib/common/notification/function.dart
+++ lib/common/notification/function.dart
@@ -6,20 +6,16 @@ import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
 Future<void> initMyNotification() async {
-  await localPlugin.initialize(
+   await localPlugin.initialize(
     initSettings,
     onSelectNotification: pushPage
   );
-
   tz.initializeTimeZones();
-
   NotificationAppLaunchDetails? details = await localPlugin.getNotificationAppLaunchDetails();
-  details ?? print("DEBUG details is null");
-  if(details != null) {
-    print("DEBUG details is not null === $details");
-  }
-  if(details?.payload != null) {
-    print("DEBUG payload is not null === ${details!.payload}");
+  details ?? debugPrint("DEBUG sasaki details is null");
+  if(details != null && details.payload != null) {
+    debugPrint("DEBUG sasaki payload is not null === ${details.payload}");
+    Navigator.of(globalCtx).pushNamed("${details.payload}");
   }
 }
 
@@ -32,7 +28,9 @@ Future<void> showMyNotification() async {
     100, 
     "local notification title", 
     "local notification body", 
-    const NotificationDetails(),
+    const NotificationDetails(
+      iOS: iOSPlatformSpecifics,
+    ),
     payload: "/next"
   );
 }
@@ -47,7 +45,9 @@ Future<void> showMyScheduledNotification() async {
     "show schedule notification title", 
     "show schedule notification body", 
     tz.TZDateTime.now(tz.local).add(const Duration(seconds: 5)),
-    const NotificationDetails(), 
+    const NotificationDetails(
+      iOS: iOSPlatformSpecifics,
+    ), 
     uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime, 
     androidAllowWhileIdle: true,
     payload: "/next"
--- lib/common/notification/plugin.dart
+++ lib/common/notification/plugin.dart
@@ -3,5 +3,17 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 final FlutterLocalNotificationsPlugin localPlugin = FlutterLocalNotificationsPlugin();
 
 const InitializationSettings initSettings = InitializationSettings(
-  iOS: IOSInitializationSettings(),
+  iOS: IOSInitializationSettings(
+    requestSoundPermission: true,
+    requestBadgePermission: true,
+    requestAlertPermission: true,
+  ),
 );
+
+const IOSNotificationDetails iOSPlatformSpecifics =
+    IOSNotificationDetails(
+        presentAlert: false,
+        presentSound: false,
+        presentBadge: true,
+        badgeNumber: 1000,
+   );
